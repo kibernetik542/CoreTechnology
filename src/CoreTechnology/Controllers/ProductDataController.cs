d17a13018c5878c26e109cb70180f5cd3593495f
kibernetik542 <qwerty5798858@box.az>
2017-05-18 13:10:02 +0400

    Add jquery scroll func-ity

---
+++ src/CoreTechnology/Controllers/ProductDataController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using CoreTechnology.Models;
+using CoreTechnology.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreTechnology.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProductDataController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductDataController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IEnumerable<ProductViewModel> LoadMoreProducts()
+        {
+            IEnumerable<Product> dbProducts = null;
+
+            dbProducts = _productRepository.Products.OrderBy(p => p.ProductId).Take(9);
+
+            List<ProductViewModel> products = new List<ProductViewModel>();
+
+            foreach (var dbProduct in dbProducts)
+            {
+                products.Add(MapDbProdToProdViewModel(dbProduct));
+            }
+            return products;
+        }
+
+        private ProductViewModel MapDbProdToProdViewModel(Product dbProduct) => new ProductViewModel()
+        {
+            ProductId = dbProduct.ProductId,
+            Name = dbProduct.Name,
+            Price = dbProduct.Price,
+            ShortDescription = dbProduct.ShortDescription,
+            ImageThumbnailUrl = dbProduct.ImageThumbnailUrl
+        };
+    }
+}
---
+++ src/CoreTechnology/ViewModels/ProductViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreTechnology.ViewModels
+{
+    public class ProductViewModel
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string ShortDescription { get; set; }
+        public decimal Price { get; set; }
+        public string ImageThumbnailUrl { get; set; }
+
+    }
+}
